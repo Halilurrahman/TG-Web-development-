99c7ab9825066ae7f737891cddc6d9c6060654d3
Halil Bagci <halilbagci75@hotmail.com>
2020-10-30 13:44:28 +0100

    onnodige react files proberen te verwijderen

---
+++ REACT/react_app_angela/src/index.js
@@ -0,0 +1,45 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// // import reportWebVitals from './reportWebVitals';
+
+
+// const yourName = "Halil Bagci";
+// var d = new Date();
+//   var n = d.getFullYear();
+// ReactDOM.render(
+  
+// <React.StrictMode>
+//     <App />
+    
+//     <div>
+//     <p>
+// Created by {yourName}. <br />
+// Copyright {n}
+//     </p>
+//     <p>
+//     Created by Angela Yu.<br />
+// Copyright 2019.
+//     </p>
+//     </div>
+//   </React.StrictMode>,
+  
+//   document.getElementById('root')
+// );
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+// reportWebVitals();
